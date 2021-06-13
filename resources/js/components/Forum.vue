d886e18cdbc283c6df2135ab0615a9b934912408
huynl <huy.nguyenle@elinext.com>
2021-06-13 09:35:19 +0700

    Add .env.dev

--- resources/js/components/Forum.vue
+++ resources/js/components/Forum.vue
@@ -3,6 +3,13 @@
         <v-row>
             <v-col cols="7">
                 <question class="mt-2" v-for="question in questions" :question="question" :key="question.id"></question>
+                <div class="text-center mt-2">
+                    <v-pagination v-if="numberOfPage"
+                                  @input="changePage"
+                        v-model="page"
+                        :length="numberOfPage"
+                    ></v-pagination>
+                </div>
             </v-col>
             <v-col>
                 <side-bar></side-bar>
@@ -20,17 +27,30 @@ export default {
     components: {Question, SideBar},
      data() {
         return {
-            questions: {}
+            questions: {},
+            page: 1,
+            numberOfPage: null,
         }
     },
     created() {
-        axios.get('/api/questions')
-            .then(res => {
-                this.questions = res.data.data
-            })
-            .catch(err => {
-                alert(err.response.statusText)
-            })
+      this.getQuestions()
+    },
+    methods:{
+        changePage(){
+            this.getQuestions(this.page)
+        },
+        getQuestions(page = 1){
+            axios.get(`/api/questions?page=${page}`)
+                .then(res => {
+                    let data = res.data
+                    this.questions = data.data
+                    this.numberOfPage = data.meta.last_page
+
+                })
+                .catch(err => {
+                    alert(err.response.statusText)
+                })
+        }
     }
 }
 </script>
